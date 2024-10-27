3edfb226363d341f08753408bf4fd8ff6ea431fd
krzysiekswol <krzysiek.swol@gmail.com>
2024-10-27 20:41:38 +0100

    init

--- src/components/typescript.component.ts
+++ src/components/typescript.component.ts
@@ -1,19 +1,25 @@
 import { ChangeDetectionStrategy, Component, WritableSignal, signal } from "@angular/core";
 
+type flightType = 'pax' | 'airTaxi';
+type privateFlight = number;
+type paxFlight = { num: number, cost: number }
+
+type Flight = Record<flightType, privateFlight | paxFlight>
+
+
 @Component({
     standalone: true,
     selector: 'bhr-typescript-practice',
     template:  `
-    <h4>Unknown</h4>
-    <p>{{ dataTs }}</p>
-    <button (click)="update()">Update</button>
+    <h4>Satifies</h4>
+    <button (click)="message()">Log</button>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TypescriptComponent {
-    data: any = 2;
-    dataTs: unknown = 2;
+export class TypescriptComponent {  
 
-    update(): void {
+    
+    message(): void {
     }
+    
 }
